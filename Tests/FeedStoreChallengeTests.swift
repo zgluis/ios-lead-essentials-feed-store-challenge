cae7af52bd574cac607ada2f7788950a0742cffe
Caio Zullo <caiozullo@gmail.com>
2021-04-29 15:50:53 +0100

    Insert cache before trying to delete to ensure the expected production code path is executed

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -122,11 +122,17 @@ class FeedStoreChallengeTests: XCTestCase, FailableFeedStoreSpecs {
 
 	func test_delete_deliversErrorOnDeletionError() throws {
 //		let stub = NSManagedObjectContext.alwaysFailingSaveStub()
-//		stub.startIntercepting()
-//
+//		let feed = uniqueImageFeed()
+//		let timestamp = Date()
 //		let sut = try makeSUT()
 //
-//		assertThatDeleteDeliversErrorOnDeletionError(on: sut)
+//		insert((feed, timestamp), to: sut)
+//
+//		stub.startIntercepting()
+//
+//		let deletionError = deleteCache(from: sut)
+//
+//		XCTAssertNotNil(deletionError, "Expected cache deletion to fail")
 	}
 
 	func test_delete_hasNoSideEffectsOnDeletionError() throws {
