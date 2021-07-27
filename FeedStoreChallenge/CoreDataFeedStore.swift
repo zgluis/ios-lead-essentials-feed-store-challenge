3bb388111c69a4ff392f23fc6cc34f325d8f648e
Luis Zapata <lalejzapata@falabella.cl>
2021-07-26 20:29:51 -0400

    Execute rollback on insertion fail

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -53,6 +53,7 @@ public final class CoreDataFeedStore: FeedStore {
 						try self.context.save()
 						completion(nil)
 					} catch {
+						context.rollback()
 						completion(error)
 					}
 				}
